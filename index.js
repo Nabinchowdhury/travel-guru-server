5294683151e57c48c7fa7f6cdbd7773e9d032e5a
Nabinchowdhury <nabinchowdhury26@gmail.com>
2022-10-23 01:46:56 +0600

    server initialized

--- index.js
+++ index.js
@@ -2,15 +2,25 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-
 app.use(cors());
 
 const port = process.env.PORT || 5000
+
+const hotels = require("./hotels.json")
+
+app.listen(port, (req, res) => {
+    console.log("Travel-guru server is running on port", port)
+})
 
 app.get("/", (req, res) => {
     res.send("Travel-guru server is running")
 })
 
-app.listen(port, (req, res) => {
-    console.log("Travel-guru server is running on port", port)
+app.get("/hotels", (req, res) => {
+    res.send(hotels)
+})
+app.get("/hotels/:id", (req, res) => {
+    const id = req.params.id
+    const hotel = hotels.find(each => each.id === id)
+    res.send(hotel)
 })
