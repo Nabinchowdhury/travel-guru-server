a7392fe8238ed4c02560674a6d47807c650de061
Nabinchowdhury <nabinchowdhury26@gmail.com>
2022-10-23 16:41:30 +0600

    updated

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.use(cors());
 
 const port = process.env.PORT || 5000
 
+const destinations = require("./destination.json")
+
 const hotels = require("./hotels.json")
 
 app.listen(port, (req, res) => {
@@ -16,11 +18,30 @@ app.get("/", (req, res) => {
     res.send("Travel-guru server is running")
 })
 
+app.get("/destinations", (req, res) => {
+    res.send(destinations)
+})
+
+app.get("/destination/:id", (req, res) => {
+    const id = req.params.id
+    const destination = destinations.find(destination => destination.id === id)
+    res.send(destination)
+})
+
 app.get("/hotels", (req, res) => {
     res.send(hotels)
 })
-app.get("/hotels/:id", (req, res) => {
+
+app.get("/destination/hotels/:id", (req, res) => {
+    const id = req.params.id
+    const destHotels = hotels.filter(hotel => hotel.destination_id === id)
+    res.send(destHotels)
+})
+
+app.get("/hotel/:id", (req, res) => {
     const id = req.params.id
     const hotel = hotels.find(each => each.id === id)
     res.send(hotel)
 })
+
+
